3382a9032cda050372edc8dcc943356426b6564c
Lyuboy83 <lyuboy83@gmail.com>
2023-04-18 22:07:00 +0300

    sem4

---
+++ Exam Block/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.ReadLine();
+
+// numbers = (2, 5, 13, 7, 6, 4);
+// size = 6;
+// sum = 0;
+// avg = 0;
+// index = 0;
+
+// while (index < size) do
+// 		if( sum = sum + numbers)
+//          (index = index = 1);
+
+//    else (avg = sum / size);
+
+// console.ReadLine [avg];
