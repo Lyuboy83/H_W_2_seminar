0fee3621459fad4ebf84a295fa6c8f0aedaaa189
Lyuboy83 <lyuboy83@gmail.com>
2023-04-20 19:08:47 +0300

    HW3

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -10,10 +10,25 @@
 
 // 23432 -> да
 
-// console.WriteLine("Введите палиндром");
-// string text_a = Console.ReadLine();
-// int chislo = Convert.ToInt32(text_a);
+// Console.WriteLine("Введите число: ");
+// string number = Console.ReadLine();
+// int len = number.Length;
 
+// if (len == 5)
+// {
+//     if (number[0] == number[4] && number[1] == number[3])
+//     {
+//         Console.WriteLine($"{number} - Палиндром");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{number} - НЕ палиндром");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+// }
 
 
 // Задача 21
@@ -25,6 +40,28 @@
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+// int x1 = ReadInt("Введите координату X первой точки: ");
+// int y1 = ReadInt("Введите координату Y первой точки: ");
+// int z1 = ReadInt("Введите координату Z первой точки: ");
+// int x2 = ReadInt("Введите координату X второй точки: ");
+// int y2 = ReadInt("Введите координату Y второй точки: ");
+// int z2 = ReadInt("Введите координату Z второй точки: ");
+
+// int A = x2 - x1;
+// int B = y2 - y1;
+// int C = z1 - z2;
+
+// double length = Math.Sqrt(A * A + B * B + C * C);
+// Console.WriteLine($"Длинна отрезка {length}");
+
+
+
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
 
 
 
@@ -35,3 +72,18 @@
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+// int number = ReadInt("Введите число N: ");
+
+// for (int i = 1; i <= number; i++)
+// { 
+//     Console.Write($"{i*i*i} ");
+// }
+
+
+
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
